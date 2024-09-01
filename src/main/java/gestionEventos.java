b3b76cc8b446b075dde1033d7d10de84f5ddf73b
Sabrina <s.lopez15@ufromail.cl>
2024-09-01 17:23:48 -0400

    Limpieza del código para usar bien los tests luego, lo borrado de la clase se almacena en el archivo bases.txt

--- src/main/java/gestionEventos.java
+++ src/main/java/gestionEventos.java
@@ -1,43 +1,16 @@
 public class gestionEventos {
     public static void main(String[] args) {
         //sala general = 10 asistentes (todos entrada general)
-        //sala VIP = 9 asistentes (3 entrada VIP con 2 invitados c/u)
-        String[][] lista = inicializarAsistentes();
-        for(int i=0;i<lista.length;i++){
-            System.out.print(lista[i][0] + " ");
-            System.out.print(lista[i][1] + " ");
-            System.out.print(lista[i][2] + " ");
-            System.out.print(lista[i][3] + " ");
-            System.out.println(lista[i][4]);
+        //sala VIP = 9 asistentes (3 entradas VIP con 2 invitados c/u)
         }
-    }
 
-    public static String[][] crearMatrizAsistentes(){
-        String[][] asistentes = new String[10][5];
-        return asistentes;
-    }
-
-    public static String[][] agregarAsistentes(String[][] asistentes, String nombre, String edad, String entrada, String invitados, String ingresadoBool){
-        for(int i=0;i<10;i++){
-            if(asistentes[i][0] == null){
-                asistentes[i][0] = nombre;
-                asistentes[i][1] = edad;
-                asistentes[i][2] = entrada;
-                asistentes[i][3] = invitados;
-                asistentes[i][4] = ingresadoBool;
-                break;
-                //Boolean.parseBoolean(String)
-                //String.valueOf(boolean)
-            }
+    //Devolver true si la persona en la fila especificada tiene 18 años o más, y false en caso contrario.
+    public static boolean verificarEdad(String[][] listaAsistentes, int fila, String edad){
+        boolean mayorDeEdad = false;
+        if(Integer.parseInt(listaAsistentes[fila][1]) >= 18){
+            mayorDeEdad = true;
         }
-        return asistentes;
-    }
-
-    public static String[][] inicializarAsistentes(){
-        String[][] asistentes = crearMatrizAsistentes();
-        asistentes = agregarAsistentes(asistentes, "1", "21", "general", "0", "false");
-        asistentes = agregarAsistentes(asistentes,"2", "21", "general", "0", "false");
-        return asistentes;
+        return mayorDeEdad;
     }
 
 }
